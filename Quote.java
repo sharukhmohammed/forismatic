5894add75e4875bd0d59141a39e3b78f54363378
Sharukh Mohammed <sharukhmohammed@me.com>
2016-04-02 11:45:30 +0530

    Added Object Orientation
    JSON is converted to Java Object of Quote Class using Google GSON.
    API request is sent via Apache HTTP libs

--- Quote.java
+++ Quote.java
@@ -1,71 +1,17 @@
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-
+package sharukh.forismatic;
 
 public class Quote
 {
-    String text;
+    public String rawJsonString;
+    public String quoteText;
+    public String quoteAuthor;
+    public String senderName;
+    public String senderLink;
+    public String quoteLink;
 
-    public Quote(String format)
+    @Override
+    public String toString()
     {
-        try
-        {
-            /*Creating Apache HTTP Client and HTTP Post Method*/
-            HttpClient httpclient = HttpClients.createDefault();
-            HttpPost httppost = new HttpPost("http://api.forismatic.com/api/1.0/");
-
-            /*Setting API towards Forismatic*/
-            List<NameValuePair> reqSetting = new ArrayList<>();
-            reqSetting.add(new BasicNameValuePair("method", "getQuote"));
-            reqSetting.add(new BasicNameValuePair("format", format));
-            //reqSetting.add(new BasicNameValuePair("key", "1"));
-            reqSetting.add(new BasicNameValuePair("lang", "en"));
-            httppost.setEntity(new UrlEncodedFormEntity(reqSetting, "UTF-8"));
-
-            /*Sending the HTTP Request*/
-            HttpResponse response = httpclient.execute(httppost);
-
-            /*Getting the Response*/
-            HttpEntity entity = response.getEntity();
-            if (entity != null)
-            {
-                InputStream inputStream;
-                inputStream = entity.getContent();
-                text = getQuoteFromStream(inputStream);
-                //TODO: Convert Input Stream into XML||JSON||String Res
-            }
-        }
-        catch (UnsupportedEncodingException | ClientProtocolException e)
-        {
-            e.printStackTrace();
-        } catch (IOException e)
-        {
-            e.getMessage();
-            e.printStackTrace();
-        }
-    }
-
-
-    private static String getQuoteFromStream(InputStream is) throws IOException
-    {
-        BufferedReader in = new BufferedReader(new InputStreamReader(is));
-        String line ;
-        StringBuilder stringBuilder = new StringBuilder();
-        while((line = in.readLine()) != null)
-        {
-            stringBuilder.append(line);
-        }
-        return stringBuilder.toString();
+        return rawJsonString;
     }
 }
